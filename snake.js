e6bcc51a8594ccf34123411a2eeb68c45cb4aca7
Squ-eeKy <143938119+Squ-eeKy@users.noreply.github.com>
2023-10-07 12:25:56 +0800

    Add files via upload

---
+++ snake.js
@@ -0,0 +1,149 @@
+//board
+var blockSize = 25;
+var rows = 20;
+var cols = 20;
+var board;
+var context;
+
+//snake head
+var snakeX = blockSize * 5;
+var snakeY = blockSize * 5;
+
+var velocityX = 0;
+var velocityY = 0;
+
+var snakeBody = [];
+
+//food
+var foodX;
+var foodY;
+
+var gameOver = false;
+
+// Username and Score
+var username = "";
+var score = 0;
+
+var gameStarted = false; // Add this variable to track if the game has started
+
+window.onload = function () {
+    board = document.getElementById("board");
+    board.height = rows * blockSize;
+    board.width = cols * blockSize;
+    context = board.getContext("2d"); //used for drawing on the board
+
+    // Hide the game board initially
+    board.style.display = "none";
+
+    // Initialize login screen
+    document.getElementById("login-screen").classList.remove("hidden");
+    document.getElementById("start-button").addEventListener("click", startGame);
+
+    // Add event listener for keyboard input
+    document.addEventListener("keyup", changeDirection);
+};
+
+function startGame() {
+    username = document.getElementById("username").value;
+    if (username.trim() === "") {
+        alert("Please enter a username.");
+    } else {
+        document.getElementById("login-screen").style.display = "none";
+        board.style.display = "block";
+        gameStarted = true; // Set the gameStarted variable to true
+        initializeGame();
+    }
+}
+
+function initializeGame() {
+    // Reset game variables
+    snakeX = blockSize * 5;
+    snakeY = blockSize * 5;
+    velocityX = 0;
+    velocityY = 0;
+    snakeBody = [];
+    score = 0;
+    gameOver = false;
+
+    // Place the initial food
+    placeFood();
+
+    // Update the score on the screen
+    document.getElementById("score").textContent = "Score: " + score;
+
+    // Start the game loop
+    setInterval(update, 1000 / 10);
+}
+
+function update() {
+    if (!gameStarted) { // Check if the game has started
+        return;
+    }
+
+    context.fillStyle = "black";
+    context.fillRect(0, 0, board.width, board.height);
+
+    context.fillStyle = "red";
+    context.fillRect(foodX, foodY, blockSize, blockSize);
+
+    if (snakeX == foodX && snakeY == foodY) {
+        snakeBody.push([foodX, foodY]);
+        placeFood();
+        score++;
+        document.getElementById("score").textContent = "Score: " + score;
+    }
+
+    for (let i = snakeBody.length - 1; i > 0; i--) {
+        snakeBody[i] = snakeBody[i - 1];
+    }
+    if (snakeBody.length) {
+        snakeBody[0] = [snakeX, snakeY];
+    }
+
+    context.fillStyle = "lime";
+    snakeX += velocityX * blockSize;
+    snakeY += velocityY * blockSize;
+    context.fillRect(snakeX, snakeY, blockSize, blockSize);
+    for (let i = 0; i < snakeBody.length; i++) {
+        context.fillRect(snakeBody[i][0], snakeBody[i][1], blockSize, blockSize);
+    }
+
+    // Game over conditions
+    if (snakeX < 0 || snakeX > cols * blockSize || snakeY < 0 || snakeY > rows * blockSize) {
+        gameOver = true;
+        endGame();
+    }
+
+    for (let i = 0; i < snakeBody.length; i++) {
+        if (snakeX == snakeBody[i][0] && snakeY == snakeBody[i][1]) {
+            gameOver = true;
+            endGame();
+        }
+    }
+}
+
+function changeDirection(e) {
+    if (e.code == "ArrowUp" && velocityY != 1) {
+        velocityX = 0;
+        velocityY = -1;
+    } else if (e.code == "ArrowDown" && velocityY != -1) {
+        velocityX = 0;
+        velocityY = 1;
+    } else if (e.code == "ArrowLeft" && velocityX != 1) {
+        velocityX = -1;
+        velocityY = 0;
+    } else if (e.code == "ArrowRight" && velocityX != -1) {
+        velocityX = 1;
+        velocityY = 0;
+    }
+}
+
+function placeFood() {
+    foodX = Math.floor(Math.random() * cols) * blockSize;
+    foodY = Math.floor(Math.random() * rows) * blockSize;
+}
+
+function endGame() {
+    alert("Game Over\n" + username + "'s score: " + score);
+    initializeGame();
+}
